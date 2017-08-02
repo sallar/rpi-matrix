9a32dd60740d9aa0e0814df4bd898af85c27f580
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-08-02 23:24:42 +0300

    Improve animations

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 chmod +x ./dist/rpi.js
-scp ./dist/rpi.js pi@192.168.11.16:/home/pi/rpi/
-ssh pi@192.168.11.16 "sudo systemctl restart rpi"
+scp ./dist/rpi.js pi@192.168.11.5:/home/pi/rpi/
+ssh pi@192.168.11.5 "sudo systemctl restart rpi"
--- src/views/clock.ts
+++ src/views/clock.ts
@@ -6,7 +6,7 @@ import chunk = require('lodash/chunk');
 
 const getPixels = require('get-pixels');
 const ms = require('ms');
-const image = require('../images/partly-cloudy-day.png');
+const image = require('../images/01d.png');
 
 const url =
   'http://api.openweathermap.org/data/2.5/weather?q=Espoo,FI&APPID=146c256a087c40880291650a75386da2&units=metric';
@@ -14,6 +14,10 @@ const store = new Store(32, 16);
 let __image: number[][] = [];
 let __sequence = 0;
 let __weatherTimer: any;
+
+const ANIM_FRAMES = 16;
+const ANIM_WIDTH = 16;
+const ANIM_HEIGHT = 16;
 
 function readImage(): Promise<any> {
   return new Promise((resolve, reject) => {
@@ -28,13 +32,13 @@ function readImage(): Promise<any> {
 }
 
 function drawSequence(store: Store, x: number, y: number, sequence = 0) {
-  const width = 16;
-  const height = 16;
-  for (let xx = 0; xx < width; xx++) {
-    for (let yy = 0; yy < height; yy++) {
-      const dx = sequence * width + xx;
+  for (let xx = 0; xx < ANIM_WIDTH; xx++) {
+    for (let yy = 0; yy < ANIM_HEIGHT; yy++) {
+      const dx = sequence * ANIM_WIDTH + xx;
       const dy = yy;
-      const [r, g, b, a]: number[] = __image[dy * 512 + dx];
+      const [r, g, b, a]: number[] = __image[
+        dy * (ANIM_WIDTH * ANIM_FRAMES) + dx
+      ];
       if (r || g || b) {
         store.drawPixel(x + xx, y + yy, Color.rgba(r, g, b, a));
       }
@@ -67,7 +71,7 @@ export function loop(): IMatrix {
 
   drawSequence(store, -1, 0, __sequence++);
 
-  if (__sequence >= 32) {
+  if (__sequence >= ANIM_FRAMES) {
     __sequence = 0;
   }
 
@@ -75,7 +79,7 @@ export function loop(): IMatrix {
 
   if (weather) {
     const temp = `${weather.main.temp}°`;
-    store.write(21, 8, temp, TomThumb, 1, Color.rgba(0, 255, 0));
+    store.write(15, 8, temp, TomThumb, 1, Color.rgba(0, 255, 0));
   }
 
   return store.matrix;
