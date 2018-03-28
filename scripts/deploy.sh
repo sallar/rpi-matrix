c9b5b7a2e27fa9d9efc8005a7ff92a64570a4c88
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-03-28 19:23:36 +0300

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+IP_ADDRESS=${RPI_ADDRESS:-192.168.11.5}
+
 chmod +x ./dist/rpi.js
-scp ./dist/rpi.js pi@192.168.11.5:/home/pi/rpi/
-ssh pi@192.168.11.5 "sudo systemctl restart rpi"
+scp ./dist/rpi.js pi@$IP_ADDRESS:/home/pi/rpi/
+ssh pi@$IP_ADDRESS "sudo systemctl restart rpi"
